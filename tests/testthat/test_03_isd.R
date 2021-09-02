0075ed206aa105b742a00de761c1a38727d23f60
Renata Diaz <diaz.renatam@gmail.com>
2021-09-02 15:14:47 -0400

    test for turnover

---
+++ tests/testthat/test_03_isd.R
@@ -0,0 +1,30 @@
+library(BBSsize)
+
+h <- BBSsize::hartland
+h_isd <- BBSsize::simulate_isd_ts(h, isd_seed = 1977)
+
+test_that("isd_overlap works without years specified", {
+
+  set.seed(1977)
+  overlap <- compare_isds(h_isd$isd)
+
+  begin_isd <- h_isd$isd %>%
+    dplyr::filter(year %in% c(1994:1998))
+
+  set.seed(1977)
+  begin_gmm <- add_gmm(begin_isd)
+
+  end_isd <- h_isd$isd %>%
+    dplyr::filter(year %in% c(2014:2018))
+
+  end_gmm <- add_gmm(end_isd)
+
+  compare_densities <- dplyr::left_join(begin_gmm, end_gmm, by = c("mass")) %>%
+    dplyr::group_by(mass) %>%
+    dplyr::mutate(minDensity = min(density.x, density.y)) %>%
+    dplyr::ungroup()
+
+  expect_true(overlap$isd_turnover[1] == 1 - sum(compare_densities$minDensity))
+
+}
+)
