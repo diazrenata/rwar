fcdebd285b5b4cc6ff780d0901a7f321f8607988
Renata Diaz <diaz.renatam@gmail.com>
2021-10-19 17:13:40 -0400

    some change to summarize_nulls

--- R/summarize_nulls.R
+++ R/summarize_nulls.R
@@ -40,16 +40,22 @@ percentile_score <- function(actual_value, null_distribution) {
 summarize_null_results <- function(null_results, actual_results) {
 
   null_use <- null_results %>%
-    dplyr::select(statenum, route, routename, isd_turnover) %>%
-    dplyr::rename(null_isd_turnover = isd_turnover) %>%
+    dplyr::select(statenum, route, routename, isd_turnover,mean_energy_raw_ratio,mean_biomass_raw_ratio) %>%
+    dplyr::rename(null_isd_turnover = isd_turnover,
+                  null_mean_energy_raw_ratio = mean_energy_raw_ratio,
+                  null_mean_biomass_raw_ratio= mean_biomass_raw_ratio) %>%
     dplyr::left_join(
-      dplyr::select(actual_results, statenum, route, routename, isd_turnover))
+      dplyr::select(actual_results, statenum, route, routename, isd_turnover, mean_biomass_raw_ratio, mean_energy_raw_ratio))
 
   null_summarized <- null_use %>%
     dplyr::group_by(statenum, route, routename) %>%
     dplyr::summarize(
       ses = ses(unique(isd_turnover), null_isd_turnover),
       perc = percentile_score(unique(isd_turnover), null_isd_turnover),
+      ses_e = ses(unique(mean_energy_raw_ratio), null_mean_energy_raw_ratio),
+      ses_b = ses(unique(mean_biomass_raw_ratio), null_mean_biomass_raw_ratio),
+      perc_e = percentile_score(unique(mean_energy_raw_ratio), null_mean_energy_raw_ratio),
+      perc_b = percentile_score(unique(mean_biomass_raw_ratio), null_mean_biomass_raw_ratio),
       nsims = dplyr::n()
     ) %>%
     dplyr::ungroup()
