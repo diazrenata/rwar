71d5fa50ea21fe3c240eb165238ef5419d61fa17
Renata Diaz <diaz.renatam@gmail.com>
2021-10-30 12:55:38 -0400

    fxns'

--- R/model_fxns.R
+++ R/model_fxns.R
@@ -105,11 +105,11 @@ loo_select <- function(some_compares) {
                                      ifelse(grepl("source", model), 2, 1))) %>%
     dplyr::arrange(matssname, currency, simtype, rank) %>%
     dplyr::group_by(matssname, currency, simtype) %>%
-    dplyr::mutate(best_elpd = elpd_loo[1],
-                  best_se = se_elpd_loo[1]) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(one_se_cutoff = best_elpd - best_se) %>%
-    dplyr::mutate(in_one_se = elpd_loo >= one_se_cutoff) %>%
+    # dplyr::mutate(best_elpd = elpd_loo[1],
+    #               best_se = se_elpd_loo[1]) %>%
+    # dplyr::ungroup() %>%
+    # dplyr::mutate(one_se_cutoff = best_elpd - best_se) %>%
+    dplyr::mutate(in_one_se = (elpd_diff + se_diff) >= 0) %>%
     dplyr::filter(in_one_se) %>%
     dplyr::group_by(currency) %>%
     dplyr::arrange(model_complexity) %>%
@@ -246,3 +246,92 @@ extract_diagnostics <- function(some_fits) {
 
   return(all_diagnostics)
 }
+
+
+#' Fit using rstanarm
+#'
+#' @param some_sims sims
+#'
+#' @return list of fits
+#' @export
+#'
+#' @importFrom rstanarm stan_glm
+fit_stanlm <- function(some_sims) {
+
+  # Fit a brm on total_energy
+  te_brm_full <- rstanarm::stan_glm(total_energy ~ (timeperiod * source) , data = some_sims, iter =8000, thin = 4)
+  te_brm_nosource <- rstanarm::stan_glm(total_energy ~ (timeperiod), data = some_sims, iter =8000, thin = 4)
+  te_brm_notime <- rstanarm::stan_glm(total_energy ~ 1, data = some_sims, iter =8000, thin = 4)
+
+  te_brms = list(
+    te_brm_full = te_brm_full,
+    te_brm_nosource = te_brm_nosource,
+    te_brm_notime = te_brm_notime
+  )
+
+
+  # Fit the brm on total_biomass
+  tb_brm_full <- rstanarm::stan_glm(total_biomass ~ (timeperiod * source) , data = some_sims, iter = 8000, thin = 4)
+  tb_brm_nosource <- rstanarm::stan_glm(total_biomass ~ (timeperiod) , data = some_sims, iter = 8000, thin = 4)
+  tb_brm_notime <- rstanarm::stan_glm(total_biomass ~ 1 , data = some_sims, iter = 8000, thin = 4)
+
+
+  tb_brms = list(
+    tb_brm_full = tb_brm_full,
+    tb_brm_nosource = tb_brm_nosource,
+    tb_brm_notime = tb_brm_notime
+  )
+
+  return(list(
+    te_brms = te_brms,
+    tb_brms = tb_brms,
+    matssname =some_sims$matssname[1],
+    simtype = some_sims$simtype[1]
+  ))
+
+}
+
+#' Compare fits
+#'
+#' @param brms_fits fits
+#'
+#' @return df
+#' @export
+#'
+#' @importFrom rstanarm loo loo_compare
+#' @importFrom dplyr mutate row_number
+compare_stanarms <- function(brms_fits) {
+
+  brms_loos<- lapply(brms_fits, rstanarm::loo, k_threshold= .7)
+
+  brms_comparison <- rstanarm::loo_compare(brms_loos) %>%
+    as.data.frame() %>%
+    dplyr::mutate(model = row.names(.),
+                  rank = dplyr::row_number())
+
+  return(brms_comparison)
+
+}
+
+#' COmpare all fits
+#'
+#' @param some_brms_fits fits
+#'
+#' @return df
+#' @export
+#'
+#' @importFrom dplyr bind_rows mutate
+compare_both_stanarms<- function(some_brms_fits) {
+
+  biomass <- compare_stanarms(brms_fits = some_brms_fits$tb_brms)
+  energy <- compare_stanarms(brms_fits = some_brms_fits$te_brms)
+
+  both_comparisons <- dplyr::bind_rows(biomass = biomass, energy = energy, .id = "currency") %>%
+    dplyr::mutate(matssname = some_brms_fits$matssname,
+                  simtype = some_brms_fits$simtype[1])
+
+
+  return(both_comparisons)
+
+}
+
