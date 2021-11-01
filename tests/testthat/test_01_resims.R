d8c18294d78371c934075523e8e53595ced95752
Renata Diaz <diaz.renatam@gmail.com>
2021-11-01 16:47:09 -0400

    sgms tests

--- tests/testthat/test_01_resims.R
+++ tests/testthat/test_01_resims.R
@@ -18,5 +18,32 @@ test_that("just_isd", {
   expect_true(unique(granby_isd$isd_seed) == 1989)
   expect_true(all(granby_isd$mass > 0))
 
+  granby_isd2 <- just_isd(g, isd_seed = 1977)
+  granby_isd3 <- just_isd(g, isd_seed = 1977)
+
+  expect_false(all(granby_isd$mass == granby_isd2$mass))
+
+  expect_true(all(granby_isd2 == granby_isd3))
+
 })
 
+test_that("sampling_gmms" {
+
+  sgmms <- construct_sampling_gmm(g, initial_isd_seed = 1989)
+  sgmms2 <- construct_sampling_gmm(g, initial_isd_seed = 1977)
+
+  sgmms3 <- construct_sampling_gmm(g, initial_isd_seed = 1977)
+
+  sgmms4 <-  construct_sampling_gmm(g)
+
+  expect_true(sum(sgmms$begin$density) == 1)
+  expect_true(sum(sgmms$end$density) == 1)
+
+  expect_true(all(sgmms$begin$mass == sgmms2$begin$mass))
+  expect_false(all(sgmms$begin$density == sgmms2$begin$density))
+
+  expect_true(all(sgmms2$begin == sgmms3$begin))
+
+  expect_false(all(sgmms2$begin$density == sgmms4$begin$density))
+
+})
